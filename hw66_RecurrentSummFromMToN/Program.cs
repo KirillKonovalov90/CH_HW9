c3f6a6040a42349cac37a09312f68a6b2a9bd843
Kirill <wakened90@gmail.com>
2023-02-02 21:06:29 +0300

    SummOfNatural works

---
+++ hw66_RecurrentSummFromMToN/Program.cs
@@ -0,0 +1,20 @@
+﻿//программа принимает 2 числа и складывает все натуральные числа
+// в промежутке от 1го до 2го числа
+
+int SummOfNaturalNumbersFromMToN(int M, int N)
+{
+    if (M >= N)
+    {
+        return M;
+    }
+
+    M = M + SummOfNaturalNumbersFromMToN(M + 1, N); 
+
+    return M;   
+}
+
+int[] nums;
+Console.Write("Введите начальный и конечный элемент для сложения через пробел: ");
+nums = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+Console.WriteLine(SummOfNaturalNumbersFromMToN(nums[0], nums[1]));
